01a0cbfae7599a7ac96b77aaaf0629052f10b58e
gray00049 <gray00049@bk.ru>
2023-03-06 15:21:19 +0300

    Remake homework

--- src/components/DateTimePretty.jsx
+++ src/components/DateTimePretty.jsx
@@ -1,24 +1,26 @@
-import DateTime from "./DateTime";
+export default function DateTimePretty(Component) {
+  function prettyTime(props) {
+    let publicationDate = new Date(props.date);
 
-export default function DateTimePretty(props) {
-  let publicationDate = new Date(props.date);
+    let minLeft = Math.floor((Date.now() - publicationDate) / 60000);
 
-  let minLeft = Math.floor((Date.now() - publicationDate) / 60000);
+    let timeLeftText;
 
-  let timeLeftText;
+    if (minLeft < 60) {
+      let timeLeft = minLeft
+      timeLeftText = `${timeLeft} минут назад`;
+    } else if (minLeft < 1440) {
+      let timeLeft = Math.floor(minLeft / 60);
+      timeLeftText = `${timeLeft} часов назад`;
+    } else { 
+      let timeLeft = Math.floor(minLeft / 1440);
+      timeLeftText = `${timeLeft} дней назад`;
+    }
 
-  if (minLeft < 60) {
-    let timeLeft = minLeft
-    timeLeftText = `${timeLeft} минут назад`;
-  } else if (minLeft < 1440) {
-    let timeLeft = Math.floor(minLeft / 60);
-    timeLeftText = `${timeLeft} часов назад`;
-  } else { 
-    let timeLeft = Math.floor(minLeft / 1440);
-    timeLeftText = `${timeLeft} дней назад`;
+    return (
+      <Component date={timeLeftText} />
+    )
   }
 
-  return (
-    <DateTime date={timeLeftText} />
-  )
+  return prettyTime;
 }
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,10 +1,13 @@
 import DateTimePretty from "./DateTimePretty";
+import DateTime from "./DateTime";
 
 export default function Video(props) {
+  const DateTimeFormatted = DateTimePretty(DateTime);
+
   return (
       <div className="video">
           <iframe src={props.url} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
-          <DateTimePretty date={props.date} />
+          <DateTimeFormatted date={props.date} />
       </div>
   )
 }
